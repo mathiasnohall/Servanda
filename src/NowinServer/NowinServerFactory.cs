139c525c6d1a0c5f6e63a6bc556a6ea5f83d9c8b
unknown <mathias.nohall@valtech.se>
2015-10-26 22:27:11 +0100

    initial commit

---
+++ src/NowinServer/NowinServerFactory.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Http.Features;
+using Microsoft.AspNet.Hosting.Server;
+using Microsoft.AspNet.Owin;
+using Microsoft.Framework.Configuration;
+using Nowin;
+
+namespace NowinServer {
+
+    public class NowinServerFactory : IServerFactory
+    {
+        private Func<IFeatureCollection, Task> _callback;
+
+        private Task HandleRequest(IDictionary<string, object> env)
+        {
+            return _callback(new OwinFeatureCollection(env));
+        }
+
+        public IFeatureCollection Initialize(IConfiguration configuration)
+        {
+            // TODO: Parse config
+            return new FeatureCollection();
+        }
+
+        public IDisposable Start(IFeatureCollection serverFeatures, Func<IFeatureCollection, Task> application)
+        {
+            var builder = ServerBuilder.New()
+                                       .SetAddress(IPAddress.Any)
+                                       .SetPort(5001)
+                                       .SetOwinApp(OwinWebSocketAcceptAdapter.AdaptWebSockets(HandleRequest));
+            _callback = application;
+            var server = builder.Build();
+            server.Start();
+            return server;
+        }
+    }
+}
+
+
+
+//public class NowinServerFactory : IServerFactory {
+
+//    private Func<IFeatureCollection, Task> callback;
+
+//    IFeatureCollection Initialize(IConfiguration configuration) {
+//        // adapt aspnet to owin app;
+//        var owinApp = OwinWebSocketAcceptAdapter.AdaptWebSockets(HandleRequest);
+
+//        // Get server info, write to console.
+//        var server = configuration["server"];
+//        var serverUrls = configuration["server.urls"];
+//        Console.WriteLine("Owin server is: {0}, listening at {1}", server, serverUrls);
+//        // parse ip address and port.
+//        var uri = new Uri(serverUrls, UriKind.Absolute);
+//        IPAddress ip;
+//        if (!IPAddress.TryParse(uri.Host, out ip)) {
+//            if (uri.Host.Equals("localhost", StringComparison.OrdinalIgnoreCase)) {
+//                ip = IPAddress.Parse("127.0.0.1");
+//            }
+//            else {
+//                ip = IPAddress.Any;
+//            }
+//        }
+//        var port = uri.Port;
+
+//        // build nowin server;
+//        var builder = ServerBuilder.New()
+//            .SetAddress(ip)
+//            .SetPort(port)
+//            .SetOwinApp(owinApp);
+
+//        var serverInfo = new NowinServerInformation(builder);
+//        return serverInfo;
+//    }
+
+//    private Task HandleRequest(IDictionary<string, object> env) {
+//        return callback(new OwinFeatureCollection(env));
+//    }
+
+//    IDisposable Start(IFeatureCollection serverFeatures, Func<IFeatureCollection, Task> application) {
+//        var info = (NowinServerInformation)serverInformation;
+//        var server = info.Builder.Build();
+//        callback = application;
+//        server.Start();
+//        return server;
+//    }
+
+//}
---
+++ src/Servanda.API/Controllers/Health.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNet.Mvc;
+
+
+namespace Servanda.API.Controllers
+{
+    [Route("api/[controller]")]
+    public class Health : Controller
+    {
+        // GET: api/values
+        [HttpGet]
+        public string Get()
+        {
+            return "OK";
+        }
+    }
+}
